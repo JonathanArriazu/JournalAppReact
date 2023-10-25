f0de6413bc39d39e8bafce1ea62e782f70627582
JonathanArriazu <joni.arriazu2@gmail.com>
2023-10-24 23:23:04 -0300

    feat: float button added to journal main page

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -2,12 +2,29 @@ import React from 'react'
 import { JournalLayout } from '../layout/JournalLayout'
 import { NothingSelectedView } from '../views'
 import { NoteView } from '../views'
+import { IconButton } from '@mui/material'
+import { AddOutlined } from '@mui/icons-material'
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       {/* <NothingSelectedView /> */}
       <NoteView />
+
+      <IconButton
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.8 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }}/>
+      </IconButton>
+
     </JournalLayout>
   )
 }
