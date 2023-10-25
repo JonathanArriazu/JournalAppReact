10576f280a36fd1dfa6655bddf8873d84ccab4a6
JonathanArriazu <joni.arriazu2@gmail.com>
2023-10-24 23:18:40 -0300

    feat: note view added to journal main page

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { JournalLayout } from '../layout/JournalLayout'
-import { NothingSelectedView } from '../views/NothingSelectedView'
+import { NothingSelectedView } from '../views'
+import { NoteView } from '../views'
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <NothingSelectedView />
+      {/* <NothingSelectedView /> */}
+      <NoteView />
     </JournalLayout>
   )
 }
