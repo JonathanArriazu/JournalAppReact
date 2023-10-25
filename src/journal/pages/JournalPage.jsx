a9f13bcc7b504d998d071a7b2e09fa7a81e60aff
JonathanArriazu <joni.arriazu2@gmail.com>
2023-10-24 22:10:00 -0300

    feat: main page layout added

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,12 @@
 import { MailOutline } from '@mui/icons-material'
 import { Typography } from '@mui/material'
 import React from 'react'
+import { JournalLayout } from '../layout/JournalLayout'
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h1'>JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ea, aspernatur pariatur esse nulla iste!</Typography>
+    </JournalLayout>
   )
 }
