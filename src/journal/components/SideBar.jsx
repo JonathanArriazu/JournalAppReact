72751fbe133512871362690c0d6905ffabde4e8e
JonathanArriazu <joni.arriazu2@gmail.com>
2023-10-25 23:32:41 -0300

    fix: show active notes in sidebar

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -2,10 +2,13 @@ import { TurnedInNot } from '@mui/icons-material'
 import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material'
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { SideBarItem } from './SideBarItem'
 
 export const SideBar = ({fixedWidth}) => {
 
     const {displayName} = useSelector(state => state.auth);
+    const {notes} = useSelector(state => state.journal);
+    console.log(notes)
 
   return (
     <Box
@@ -29,18 +32,8 @@ export const SideBar = ({fixedWidth}) => {
 
             <List>
                 {
-                    ['Enero', 'Febrero', 'Marzo', 'Abril'].map( text => (
-                        <ListItem key={ text } disablePadding>
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    <TurnedInNot />
-                                </ListItemIcon>
-                                <Grid container>
-                                    <ListItemText primary={text}/>
-                                    <ListItemText secondary={'lore asdasd asdasd asdasd asdasd asdasdasd asdasdasd asd'}/>
-                                </Grid>
-                            </ListItemButton>
-                        </ListItem>
+                    notes.map( note => (
+                        <SideBarItem key={ note.id } {...note} />
                     ) )
                 }
             </List>
